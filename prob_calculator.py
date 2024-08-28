f0282d5069fea2db0cc20b2421daf4fc2d6d4a30
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-08-28 14:56:45 +0800

    drafting "draw" method

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,18 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, *kwargs):
+        pass
 
+    def draw(self, num_balls):
+        #ball_box = [ list containing balls ]
+        #drawn_list = []
+        #for n in num_balls:
+        #   random_ball = random.choice(ball_box)
+        #   ball_box.remove(random_ball)
+        #   drawn_list.append(random_ball)
+
+        pass
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
