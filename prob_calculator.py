cb46c942350a6449d9cb2ff91cf3b0df1668e7b5
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-09-01 17:10:28 +0800

    working using test_module.py
    this code passes test_module.py , but does not pass the freeCodeCamp browser test, with error: The draw method should behave correctly when the number of balls to extract is bigger than the number of balls in the hat.

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,9 +1,7 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
-
     def __init__(self, **kwargs):
         self.contents = []
         for color,number in kwargs.items():
@@ -24,46 +22,33 @@ class Hat:
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     matches = 0
-    experiments = 0
-    expected_balls_list = []
-
-    for key,value in expected_balls.items():
-        for n in range(value):
-            expected_balls_list.append(key)
 
     for i in range(num_experiments):
-        color_true = 0
+        color_match = 0
         hat_reset = copy.deepcopy(hat)
         draw = hat_reset.draw(num_balls_drawn)
+      
+        #Uncomment to see what balls are drawn and what are expected:
         #print(f'\n\n--DRAW-- {draw}')
-        #print(f'Expected Balls: {expected_balls}')
+        #print(f'Balls we want to match: {expected_balls}')
+
         for color, number in expected_balls.items():
-            ball_count_color=0
+            balls_per_color=0
 
             for ball in draw:
                 if (color==ball):
-                    ball_count_color+=1
-            #print(f'{color} ball count: {ball_count_color}')
+                    balls_per_color+=1
+            #print(f'{color} balls: {balls_per_color}')
 
-            if ball_count_color >= number:
-                color_true+=1
-                #print (f'color_true so far: {color_true}')    
-        if color_true == len(expected_balls):
+            if balls_per_color >= number:
+                color_match+=1
+                 
+        if color_match == len(expected_balls):
             matches+=1
-        #print(f'target value: {len(expected_balls)}')     
-        #print(f'matches = {matches}')
-        experiments +=1
 
-    probability = matches/experiments
-    #print (f'probability= {probability}')
+        #print(f'matches so far : {matches}')
+
+    probability = matches/num_experiments
+    #print (f'probability= {probability}\nnumber of experiments: {num_experiments}\nmatches: {matches}\n')
     return probability
-    #print(f'number of experiments: {experiments}\nmatches: {matches}\n')
 
-#hat1 = Hat(blue=5, red=5, green=5)
-#experiment(hat1, {"red": 1, "blue": 1}, num_balls_drawn=3, num_experiments=100)
-
-#hat = Hat(black=6, red=4, green=3)
-#probability = experiment(hat=hat,
-#                  expected_balls={'red':2,'green':1},
-#                  num_balls_drawn=5,
-#                  num_experiments=2000)
